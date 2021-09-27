c344a333ceda6fcb2ffa593b83242db3cf5c008d
KrishKonda <77596750+KrishKonda@users.noreply.github.com>
2021-09-26 20:49:29 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -15,3 +15,21 @@ function take_snapshot(){
     });
     
 }
+console.log("ml5 version:",ml5.version);
+classifier=ml5.imageClassifier('MobileNet',modelLoaded);
+function modelLoaded(){
+    console.log("model loaded");
+}
+function check(){
+    img=document.getElementById("captured_image");
+    classifier.classify(img,gotResult);
+}
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+    document.getElementById("object_name").innerHTML=results[0].label;
+    }
+}
